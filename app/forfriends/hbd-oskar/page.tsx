3e92543cde90632d4457af479105df6fddae5a22
remi-higuchi_hmgroup <remi.higuchi@hm.com>
2023-12-06 04:10:03 +0100

    gooood

--- app/forfriends/hbd-oskar/page.tsx
+++ app/forfriends/hbd-oskar/page.tsx
@@ -1,12 +1,13 @@
+'use client'
 import React from 'react';
-import {Card, CardBody} from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
 
-import type { Metadata } from 'next';
+// import type { Metadata } from 'next';
 
-export const metadata: Metadata = {
-    title: 'HBD OSKAR | REMI HIGUCHI',
-    description: 'birthday fika tickets for oskar',
-}
+// export const metadata: Metadata = {
+//     title: 'HBD OSKAR | REMI HIGUCHI',
+//     description: 'birthday fika tickets for oskar',
+// }
 
 export default function HbdOskar() {
 
@@ -16,6 +17,8 @@ export default function HbdOskar() {
         { id: 2, used: false, date: 'XX-XX-XX', place: 'TBD' },
         { id: 3, used: false, date: 'XX-XX-XX', place: 'TBD' },
     ]
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+    // const { isOpen, onOpen } = useDisclosure();
 
     return (
         <div className="">
@@ -24,13 +27,13 @@ export default function HbdOskar() {
                 {tickets.map((ticket =>
                     <div key={ticket.id} className='border-1 border-black min-w-[250px]'>
                         {ticket.used ? (
-                            <div className='bg-neutral-400'>
+                            <div className='bg-neutral-300'>
                                 <p className='px-2 text-white bg-black text-sm'>VALID TILL 2024-10-23</p>
                                 <div className='p-2'>
                                     <p className='py-1'>FIKA TICKET #{ticket.id}</p>
                                     <p className='text-sm'>DATE: {ticket.date}</p>
                                     <p className='text-sm'>PLACE: {ticket.place}</p>
-                                    <p className='my-2 border-1 border-black text-sm py-1 px-6 text-center'>USED</p>
+                                    <Button isDisabled="true" radius="none" size='sm' className='w-full my-2 border-1 bg-transparent border-black text-sm py-1'>USED</Button>
                                 </div>
                             </div>
                         ) : (
@@ -40,9 +43,28 @@ export default function HbdOskar() {
                                     <p className='py-1'>FIKA TICKET #{ticket.id}</p>
                                     <p className='text-sm'>DATE: {ticket.date}</p>
                                     <p className='text-sm'>PLACE: {ticket.place}</p>
-                                    <button className='w-full my-2 border-1 border-black text-sm py-1 px-6 hover:bg-black hover:text-white duration-200'>
+                                    <Button onPress={onOpen} radius="none" size='sm' className='w-full my-2 border-1 bg-transparent border-black text-sm py-1 hover:bg-black hover:text-white duration-200'>
                                         LET&apos;S HAVE FIKA →
-                                    </button>
+                                    </Button>
+                                    <Modal placement='bottom' isOpen={isOpen} onOpenChange={onOpenChange}>
+                                        <ModalContent>
+                                            {(onClose) => (
+                                                <>
+                                                    <ModalHeader className="flex flex-col gap-1">HEJ :)</ModalHeader>
+                                                    <ModalBody>
+                                                        <p>
+                                                            TEXT MEEEEE
+                                                        </p>
+                                                    </ModalBody>
+                                                    <ModalFooter>
+                                                        <Button onPress={onClose}>
+                                                            OK
+                                                        </Button>
+                                                    </ModalFooter>
+                                                </>
+                                            )}
+                                        </ModalContent>
+                                    </Modal>
                                 </div>
                             </div>
                         )}
