14c914ca943f9593dd220ac270b5b6e6f2f1f114
remi-higuchi_hmgroup <remi.higuchi@hm.com>
2023-12-08 16:46:33 +0100

    aaaa

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,29 @@
 import Image from 'next/image'
 
 import type { Metadata } from 'next'
- 
+
 export const metadata: Metadata = {
   title: 'REMI HIGUCHI',
   description: 'welcome to remihiguchi.com',
 }
- 
+
 export default function Home() {
   return (
-    <main className="">
-      <h1 className='text-3xl'>Hi! I&apos;m Remi</h1>
-      <p>TOKYO ⇔ STOCKHOLM</p>
-      <p>I enjoy software engineering and product design.</p>
+    <main className="flex">
+      <div>
+        <h1 className='text-3xl'>Hi! I&apos;m Remi</h1>
+        <p>TOKYO ⇔ STOCKHOLM</p>
+        <p>I enjoy software engineering and product design.</p>
+      </div>
+      <div >
+        <Image
+          src="/images/stockholm.jpg"
+          width={800}
+          height={300}
+          alt="Picture of Stockholm"
+          style={{ objectFit: "contain" }}
+        />
+      </div>
     </main>
   )
 }
